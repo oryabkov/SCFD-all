260aeec818b65953dd833d794ba1455e0ba6de69
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2023-10-25 15:29:04 +0300

    added log_mpi and log_mpi basic files into utils (not tested, as is)

---
+++ include/scfd/utils/log_mpi_basic.h
@@ -0,0 +1,66 @@
+// Copyright © 2016-2018 Ryabkov Oleg Igorevich, Evstigneev Nikolay Mikhaylovitch
+
+// This file is part of SimpleCFD.
+
+// SimpleCFD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2 only of the License.
+
+// SimpleCFD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with SimpleCFD.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef __SCFD_UTILS_LOG_MPI_BASIC_H__
+#define __SCFD_UTILS_LOG_MPI_BASIC_H__
+
+#include <string>
+#include <exception>
+#include <stdexcept>
+#include <cstdarg>
+#include <cstdio>
+#include <mpi.h>
+#include "log.h"
+
+namespace scfd
+{
+namespace utils
+{
+
+class log_mpi_basic
+{
+    int     log_lev;
+    int     comm_rank_, comm_size_;
+public:
+    log_mpi_basic() : log_lev(1) 
+    {  
+        if (MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank_) != MPI_SUCCESS) throw std::runtime_error("log_mpi_basic::MPI_Comm_rank failed");
+        if (MPI_Comm_size(MPI_COMM_WORLD, &comm_size_) != MPI_SUCCESS) throw std::runtime_error("log_mpi_basic::MPI_Comm_size failed");
+    }
+
+    void msg(const std::string &s, t_msg_type mt = INFO, int _log_lev = 1)
+    {
+        if ((mt != ERROR)&&(_log_lev > log_lev)) return;
+        //TODO
+        if (mt == INFO) {
+            if (comm_rank_ == 0) printf("INFO:    %s\n", s.c_str());
+        } else if (mt == INFO_ALL) {
+            printf("INFO(%2d):%s\n", comm_rank_, s.c_str());
+        } else if (mt == WARNING) {
+            printf("WARNING(%2d): %s\n", comm_rank_, s.c_str());
+        } else if (mt == ERROR) {
+            printf("ERROR(%2d):   %s\n", comm_rank_, s.c_str());
+        } else 
+            throw std::logic_error("log_mpi_basic::log: wrong t_msg_type argument");
+    }
+    void set_verbosity(int _log_lev = 1) { log_lev = _log_lev; }
+};
+
+}
+
+}
+
+#endif
