a3f45d45b5863bfb9b683293967ed45352513a58
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2025-03-29 19:08:07 +0300

    added comments + missed copyrights

--- include/scfd/communication/rect_partitioner.h
+++ include/scfd/communication/rect_partitioner.h
@@ -1,3 +1,19 @@
+// Copyright © 2023-2025 Ryabkov Oleg Igorevich, Evstigneev Nikolay Mikhaylovitch
+
+// This file is part of SCFD.
+
+// SCFD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2 only of the License.
+
+// SCFD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with SCFD.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef __SCFD_RECT_PARTITIONER_H__
 #define __SCFD_RECT_PARTITIONER_H__
 
@@ -6,6 +22,8 @@
 #include <scfd/static_vec/vec.h>
 #include <scfd/static_vec/rect.h>
 #include "mpi_comm_info.h"
+
+//TODO add Comm template parameter, rename _t to _type, rename ordinal,big_ordinal with _type
 
 namespace scfd
 {
