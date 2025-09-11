f01e8b3fca8ebdc133f255864fd62ab4e9239905
evstigneevnm <evstigneevnm@gmail.com>
2025-09-11 22:12:58 +0300

    format fixing

--- test/communication/test_mpi_comm_split.cpp
+++ test/communication/test_mpi_comm_split.cpp
@@ -23,17 +23,17 @@ int main(int argc, char *argv[])
 {
     using mpi_comm_t = scfd::communication::mpi_comm;
     using mpi_comm_info_t = typename mpi_comm_t::mpi_comm_info_type;
+    using log_t = scfd::utils::log_mpi;
+    using mpi_wrap_t = scfd::communication::mpi_wrap;
 
-    scfd::communication::mpi_wrap mpi(argc, argv);
+    mpi_wrap_t mpi(argc, argv);
     mpi_comm_info_t comm_info = mpi.comm_world();
-    
-    scfd::utils::log_mpi log;
+    log_t log;
 
     auto num_procs = comm_info.num_procs;
     auto my_id = comm_info.myid;
     
     int color = my_id/2;
-
     mpi_comm_t comm1 = comm_info.split(color);
     int numproc1 = comm1.num_procs();
     int my_id1 = comm1.myid();
@@ -43,13 +43,13 @@ int main(int argc, char *argv[])
     auto res = comm_info.all_reduce_sum(1);
     auto res_all = comm_info.all_reduce_sum(res1);
     int res_all_ref = 2*num_procs;
+
     if(num_procs%2 == 1)
     {
         res_all_ref = 2*num_procs - 1;
     }
     
     log.info_f("res_all = %i, res_all_ref = %i", res_all, res_all_ref);
-
     if(res_all_ref != res_all)
     {
         log.error("FAILED");
