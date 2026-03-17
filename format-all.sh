80cff0e807577327f69d6b86883114ad64ff9d17
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2026-03-18 02:10:19 +0300

    add .clang-format and script to format all source files

---
+++ format-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#// Source - https://stackoverflow.com/a/36046965
+#// Posted by Antimony, modified by community. See post 'Timeline' for change history
+#// Retrieved 2026-03-18, License - CC BY-SA 4.0
+
+#find foo/bar/ -iname '*.h' -o -iname '*.cpp' | xargs clang-format -i
+
+find include/ -iname '*.h' -o -iname '*.cpp' -o -iname '*.cu' | xargs clang-format-15 -i
+find test/ -iname '*.h' -o -iname '*.cpp' -o -iname '*.cu' | xargs clang-format-15 -i
